25a59059a45892fd1b6406e1cd21094eb527d834
Beyza Subaşı <44063374+beyzasubasi@users.noreply.github.com>
2021-03-14 11:42:18 +0300

    Create giris.php

---
+++ giris.php
@@ -0,0 +1,60 @@
+<?php require_once 'baglan.php';  //2)giriş işlemi
+
+
+$kadi    =trim($_POST['kadi']);
+$parola  =trim($_POST['parola']); //
+
+
+$sorgu =  $db->prepare("SELECT * FROM users WHERE kadi=? and parola=?");
+$sorgu -> execute(array($kadi, $parola)); //kontrol ettirdim değişkenlerimi
+$islem =  $sorgu->fetch();
+
+if($sorgu->rowCount())
+{
+	$_SESSION['users_id'] = $islem['id'];  //ya burada ya da offers 75te yapılabilir 
+	$_SESSION['ad']       = $islem['ad'];
+	$_SESSION['soyad']    = $islem['soyad'];
+	$_SESSION['kadi']     = $islem['kadi'];
+
+}
+header("location: giris2.php");
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
