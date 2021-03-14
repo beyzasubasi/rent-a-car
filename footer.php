af8527768f6562e5106d319883f249ea230d6883
Beyza Subaşı <44063374+beyzasubasi@users.noreply.github.com>
2021-03-14 11:42:16 +0300

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,77 @@
+<footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3 footer-item">
+            <h4>KURUMSAL</h4>
+            <p>Şirket Profili</p>
+            <p>Ofislerimiz</p>
+            <p>Kampanyalar</p>
+            <p>Basından Haberler</p>
+            <p>Kişisel Verilerin Korunması</p>
+            <ul class="social-icons">
+              <li><a rel="nofollow" href="#" target="_blank"><i class="fa fa-facebook"></i></a></li>
+              <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+              <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+            </ul>
+          </div>
+          <div class="col-md-3 footer-item">
+            <h4>MÜŞTERİ HİZMETLERİ</h4>
+            <p>Gizlilik Politikası</p>
+            <p>Yasal Uyarı</p>
+            <p>İptal/İade Şartları</p>
+            <p>Kiralama Sözleşmesi</p>
+            <p>SSS</p>
+
+          </div>
+          <div class="col-md-3 footer-item">
+            <h4>ETKİLEŞİM SAYFA</h4>
+            <ul class="menu-list">
+              <li><a href="#">Anasayfa</a></li>
+              <li><a href="#">Filomuz</a></li>
+              <li><a href="#">Rezervasyon</a></li>
+              <li><a href="#">Hakkımızda</a></li>
+              <li><a href="#">Giriş yap</a></li>
+              <li><a href="#">Bize Ulaşın</a></li>
+            </ul>
+          </div>
+          <div class="col-md-3 footer-item last-item">
+            <h4>BİZE ULAŞIN</h4>
+            <div class="contact-form">
+              <form id="contact footer-contact" action="" method="post">
+                <div class="row">
+                  <div class="col-lg-12 col-md-12 col-sm-12">
+                    <fieldset>
+                      <input name="name" type="text" class="form-control" id="name" placeholder="Ad/Soyad">
+                    </fieldset>
+                  </div>
+                  <div class="col-lg-12 col-md-12 col-sm-12">
+                    <fieldset>
+                      <input name="email" type="text" class="form-control" id="email" pattern="[^ @]*@[^ @]*" placeholder="E-Mail Adres">
+                    </fieldset>
+                  </div>
+                  <div class="col-lg-12">
+                    <fieldset>
+                      <textarea name="message" rows="6" class="form-control" id="message" placeholder="Mesajınız"></textarea>
+                    </fieldset>
+                  </div>
+                  <div class="col-lg-12">
+                    <fieldset>
+                      <button type="submit" id="form-submit" class="filled-button">GÖNDER</button>
+                    </fieldset>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <div class="sub-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+          </div>
+        </div>
+      </div>
+    </div>
