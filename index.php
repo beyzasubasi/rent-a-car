666f289fe3ec1279790ab5c206ccb0ab99e2966d
Beyza Subaşı <44063374+beyzasubasi@users.noreply.github.com>
2021-03-14 11:42:25 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,343 @@
+<?php require_once 'baglan.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+
+    <title></title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+  </head>
+
+  <body>
+
+    <!-- ***** Preloader Start ***** -->
+    <div id="preloader">
+        <div class="jumper">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>  
+    <!-- ***** Preloader End ***** -->
+
+    <!-- Header -->
+    <div class="sub-header">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-xs-12">
+            <ul class="left-info">
+              <li><a href="#"><i class="fa fa-envelope"></i>ventacav@company.com</a></li>
+              <li><a href="#"><i class="fa fa-phone"></i>0000-000-0000</a></li>
+            </ul>
+          </div>
+          <div class="col-md-4">
+            <ul class="right-icons">
+              <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+              <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+              <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <?php require_once 'header.php'; ?>
+
+    <!-- Page Content -->
+    <!-- Banner Starts Here -->
+    <div class="main-banner header-text" id="top">
+        <div class="Modern-Slider">
+          <!-- Item -->
+          <div class="item item-1">
+            <div class="img-fill">
+                <div class="text-content">
+                  <h4>KIRMIZI IŞIKTA FARKEDİLMEK İSTER MİSİNİZ? <br></h4>
+                  <h6>O ZAMAN DOĞRU YERDESİNİZ</h6>
+                  <p>HEMEN FİLOMUZA GÖZ ATIN!</p>
+                  <a href="fleet.php" class="filled-button">FİLOMUZ</a>
+                </div>
+            </div>
+          </div>
+          <!-- // Item -->
+          <!-- Item -->
+          <div class="item item-2">
+            <div class="img-fill">
+                <div class="text-content">
+                  <h6>ÖNERİ VE İSTEKLERİNİZE AÇIĞIZ</h6>
+                  <h4>SİZE ÖZEL AYRICALIKLARIMIZ VAR! <br></h4>
+                  <p>7/24 MÜŞTERİ DESTEĞİMİZLE!</p>
+                  <a href="contact.php" class="filled-button">BİZE ULAŞIN</a>
+                </div>
+            </div>
+          </div>
+          <!-- // Item -->
+          <!-- Item -->
+          <div class="item item-3">
+            <div class="img-fill">
+                <div class="text-content">
+                  <h6>İŞTE BU!</h6>
+                  <h4>IŞILTILI BİR SÜRÜŞ DENEYİMİ İÇİN<br></h4>
+                  <p>REZERVASYON SEÇENEKLERİMİZE GÖZ ATIN!</p>
+                  <a href="offers.php" class="filled-button">REZERVASYON</a>
+                </div>
+            </div>
+          </div>
+          <!-- // Item -->
+        </div>
+    </div>
+    <!-- Banner Ends Here -->
+
+    <div class="request-form">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8">
+            <h4>Anında geri aranmak ister misiniz?</h4>
+            <span>Bilgilendirilmek isterseniz iletişim bilgilerinizi bırakın, size geri dönüş yapalım.</span>
+          </div>
+          <div class="col-md-4">
+            <a href="contact.php" class="border-button">SİZİ ARAYALIM</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
+    <div class="services">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="section-heading">
+              <h2><em>TEKLİFLERİMİZ</em></h2>
+              <span>ONLINE REZERVASYON SEÇENEĞİ İLE..</span>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="service-item">
+              <img src="assets/images/EnHS8EEVkAYTPr1.jpg" alt="">
+              <div class="down-content">
+                <h4>MERCEDES-AMG GT 4.0R</h4>
+                <div style="margin-bottom:10px;">
+                  <span>Günlük 600 TL'den başlayan fiyatlarla</span>
+                </div>
+                <p><b>1AYLIK+ PAKET    :</b> 600TL   günlük kira bedeli</p>
+                <p><b>3HAFTALIK PAKET  :</b> 700TL   günlük kira bedeliL</p>
+                <p><b>2HAFTALIK PAKET  :</b> 800TL   günlük kira bedeli</p>
+                <p><b>1 HAFTALIK PAKET :</b> 900TL   günlük kira bedeli</p>
+                <p><b>1-3 GÜNLÜK PAKET :</b> 1.000TL günlük kira bedeli</p>
+                <a href="fleet.php" class="filled-button">ARACI İNCELE!</a>
+              </div>
+            </div>
+            <br>
+          </div>
+          <div class="col-md-4">
+            <div class="service-item">
+              <img src="assets/images/1b.jpg" alt="">
+              <div class="down-content">
+                <h4>LAMBORGHINI-URUS 4.0</h4>
+                <div style="margin-bottom:10px;">
+                  <span>Günlük 2.000 TL'den başlayan fiyatlarla</span>
+                </div>
+                <p><b>1AYLIK+ PAKET    :</b> 2.000TL günlük kira bedeli</p>
+                <p><b>3HAFTALIK PAKET  :</b> 2.100TL günlük kira bedeli</p>
+                <p><b>2HAFTALIK PAKET  :</b> 2.200TL günlük kira bedeli</p>
+                <p><b>1 HAFTALIK PAKET :</b> 2.300TL günlük kira bedeli</p>
+                <p><b>1-3 GÜNLÜK PAKET :</b> 2.400TL günlük kira bedeli</p>
+                <a href="fleet.php" class="filled-button">ARACI İNCELE!</a>
+              </div>
+            </div>
+            <br>
+          </div>
+          <div class="col-md-4">
+            <div class="service-item">
+              <img src="assets/images/EozpNN-XMAIMTl7.jpg" alt="">
+              <div class="down-content">
+                <h4>BMW-2 216D GRAN COUPE</h4>
+                <div style="margin-bottom:10px;">
+                  <span>Günlük 400 TL'den başlayan fiyatlarla</span>
+                </div>
+                <p><b>1AYLIK+ PAKET    :</b> 400TL günlük kira bedeli</p>
+                <p><b>3HAFTALIK PAKET  :</b> 500TL günlük kira bedeli</p>
+                <p><b>2HAFTALIK PAKET  :</b> 600TL günlük kira bedeli</p>
+                <p><b>1 HAFTALIK PAKET :</b> 700TL günlük kira bedeli</p>
+                <p><b>1-3 GÜNLÜK PAKET :</b> 800TL günlük kira bedeli</p>
+                <a href="fleet.php" class="filled-button">ARACI İNCELE!</a>
+              </div>
+            </div>
+            <br>
+          </div>
+          <div class="col-md-4">
+            <div class="service-item">
+              <img src="assets/images/2b.jpg" alt="">
+              <div class="down-content">
+                <h4>ASTON MARTIN DB11 COUPE</h4>
+                <div style="margin-bottom:10px;">
+                  <span>Günlük 1200 TL'den başlayan fiyatlarla</span>
+                </div>
+                <p><b>1AYLIK+ PAKET    :</b> 1.200TL günlük kira bedeli</p>
+                <p><b>3HAFTALIK PAKET  :</b> 1.300TL günlük kira bedeli</p>
+                <p><b>2HAFTALIK PAKET  :</b> 1.400TL günlük kira bedeli</p>
+                <p><b>1 HAFTALIK PAKET :</b> 1.500TL günlük kira bedeli</p>
+                <p><b>1-3 GÜNLÜK PAKET :</b> 1.600TL günlük kira bedeli</p>
+                <a href="fleet.php" class="filled-button">ARACI İNCELE!</a>
+              </div>
+            </div>
+            <br>
+          </div>
+          <div class="col-md-4">
+            <div class="service-item">
+              <img src="assets/images/2b2f35c511fef6fb5a9cccc8210e6816.jpg" alt="">
+              <div class="down-content">
+                <h4>RANGE ROVER 3.0 SDV6 HSE</h4>
+                <div style="margin-bottom:10px;">
+                  <span>Günlük 900 TL'den başlayan fiyatlarla</span>
+                </div>
+                <p><b>1AYLIK+ PAKET    :</b> 900TL günlük kira bedeli</p>
+                <p><b>3HAFTALIK PAKET  :</b> 1.000TL günlük kira bedeli</p>
+                <p><b>2HAFTALIK PAKET  :</b> 1.050TL günlük kira bedeli</p>
+                <p><b>1 HAFTALIK PAKET :</b> 1.100TL günlük kira bedeli</p>
+                <p><b>1-3 GÜNLÜK PAKET :</b> 1.200TL günlük kira bedeli</p>
+                <a href="fleet.php" class="filled-button">ARACI İNCELE!</a>
+              </div>
+            </div>
+            <br>
+          </div>
+          <div class="col-md-4">
+            <div class="service-item">
+              <img src="assets/images/aa.jpg" alt="">
+              <div class="down-content">
+                <h4>AUDI R8 4.2FSI QUATTRO</h4>
+                <div style="margin-bottom:10px;">
+                  <span>Günlük 900 TL'den başlayan fiyatlarla</span>
+                </div>
+                <p><b>1AYLIK+ PAKET    :</b> 900TL   günlük kira bedeli</p>
+                <p><b>3HAFTALIK PAKET  :</b> 1.000TL günlük kira bedeli</p>
+                <p><b>2HAFTALIK PAKET  :</b> 1.100TL günlük kira bedeli</p>
+                <p><b>1 HAFTALIK PAKET :</b> 1.200TL günlük kira bedeli</p>
+                <p><b>1-3 GÜNLÜK PAKET :</b> 1.300TL günlük kira bedeli</p>
+                <a href="fleet.php" class="filled-button">ARACI İNCELE!</a>
+              </div>
+            </div>
+            <br>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="fun-facts">
+      <div class="container">
+        <div class="more-info-content">
+          <div class="row">
+            <div class="col-md-6">
+            </div>
+            <div class="col-md-6 align-self-center">
+              <div class="right-content">
+                <span>BİZ KİMİZ?</span>
+                <h2><em>Şirketimiz</em> Hakkında Bilgi Edinin</h2>
+                <p>Rent a car hizmeti hem bireyler hem de kurumlar için giderek daha fazla önem kazanan bir ihtiyaç haline geldi. Turizm algısının gelişmesi ve havalimanı sayısının artması ile hem uzun hem de kısa dönem araç kiralamada talep arttı. VENT A CAV bu noktada kullanıcıya kiralık araba seçimini kolaylaştıran bir hizmet sağlıyor. Çok sayıda yerel ve global markanın sunduğu ayrıcalıklalı hizmetlerin arasından seçim yapmak kullanıcı için oldukça zorlayıcı. Bununla beraber en uygun fiyatlı araç kşralama seçeneğinin, hangi marka ya da ofis tarafından sunulduğunu araştırmak da çok zahmetli olabiliyor. İşte tam da bu ihtiyacı karşılama fikrinden yola çıkan VENTACAV.com sistemli ve güvenli bir yapı ile sunduğu rent a car fiyat karşılaştırma sistemiyle kullanıcının aradığını hızlı ve zahmetsizce bulmasına odaklanıyor. VENT A CAR ile araç kiralamak çok daha kolay!</p>
+                <a href="about.php" class="filled-button">DEVAMI..</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="more-info">
+      <div class="container">
+        <div class="section-heading">
+          <h2><em>Bloğumuzu</em> Okuyun</h2>
+          <span>ARAÇ KİRALAMANIN PÜF NOKTALARI</span>
+        </div>
+
+        <div class="row" id="tabs">
+          <div class="col-md-4">
+            <ul>
+              <li><a href='#tabs-1'>KİRALIK ARAÇ NEDEN TERCİH EDİLİYOR? <br> <small>Melisa Kaygın &nbsp;|&nbsp; 31.12.2020 04:38</small></a></li>
+              <li><a href='#tabs-2'>ARAÇ KİRALAMAK NEDEN AVANTAJLIDIR? <br> <small>Asya Demir &nbsp;|&nbsp; 15.10.2020 15:43</small></a></li>
+              <li><a href='#tabs-3'>ARABA ALMAK MI ARABA KİRALAMAK MI?<br> <small>Aras Karcıoğlu &nbsp;|&nbsp; 03.12.2020 18:10</small></a></li>
+               <li><a href='#tabs-4'>ARAÇ KİRALAMA HİZMETİ NELERİ KAPSAR? <br> <small>Emir Erkin &nbsp;|&nbsp; 27.07.2020 10:10</small></a></li>
+              <li><a href='#tabs-5'>KİRALIK ARABA KARŞILAŞTIRILMASI NASIL YAPILMALI? <br> <small>Arda Keskin &nbsp;|&nbsp; 07.09.2020 07:55</small></a></li>
+            </ul>
+
+            <br>
+            <br>
+          </div>
+
+          <div class="col-md-8">
+            <section class='tabs-content'>
+
+
+              <article id='tabs-1'>
+                <img src="assets/images/original.jpg" alt="">
+                <h4><a href="blog-details.php">Trafikteki kiralık araç sayısı her geçen gün artıyor. Buradaki en önemli faktör araç satın almanın giderek sorlaşması.</a></h4>
+                <p>Araç satın almak, sigorta-kasko ve vergiler de göz önünde bulundurulduğunda satın alma maliyetinden daha kabarık bilançolar ortaya çıkartıyor. Bu yüzden kiralık araç kısa ve uzun vadeli araç ihtiyacçları olduğu dönemlerde kiralık araç ile çözüm yaratıyorlar. Kiralık araç yani oto kiralama kullanmak sizi vergi, sigorta, kasko gibi masraflardan kurtardığı gibi satın alma aşamasında maddi imkanlarınızın yetmediği segmentte araçlar kullanabilmenizi de sağlar.</p>
+              </article>
+              <article id='tabs-2'>
+                <img src="assets/images/3ffca3d81f5f7dfa9d0097a833a752f2.jpg" alt="">
+                <h4><a href="blog-details.php">Ülkemizde de giderek daha fazla farkına varıldığı üzere araba kiralamak, çoğu zaman araç satın almaktan daha karlı olabilir.</a></h4>
+                <p>Bunun nedeni, kiralık araç fiyatları belirlenirken bu rakama aracın bakımı, temizliği, olası bir arıza ya da hasar için masrafların sağlanması, yedek raçla hemen değişimin yapılması, vergisi, periyodik bakımları, sigorta gibi kalemlerin de dahil olmasıdır. Ayrıca bu süreçlerle birey ya da şirket bazında uğraşmak zorunda olmamakla da önemli bir konfor sunar.</p>
+              </article>
+              <article id='tabs-3'>
+                <img src="assets/images/2b.jpg" alt="">
+                <h4><a href="blog-details.php">Ülkemizde artan vergi yüküyle birlikte sıfır kilometre araba almak oldukça zorlaştı.</a></h4>
+                <p>Değerinin çok üzerinde rakamlar ödeyerek yeni bir araba almak, oldukça yüksek bir miktar birikim yapmayı ya da zorlu kredileri göze almayı gerektiriyor. Durum böyle olunca hayalinizdeki marka ve modeller arasında seçim yapamıyorsanız belirlediğiniz araçlar için oto kiralamak yaparak uzun süre deneme şansınız olacaktır. Böylece ihtiyacınızı gerçekten hangi marka/model aracın karşıladığından emin olarak satın alma sürecinde daha doğru ve sağlıklı bir biçimde ilerleyebilirsiniz.</p>
+              </article>
+              <article id='tabs-4'>
+                <img src="assets/images/f20847_7b8e470752de4c6ab38a1ffde009cb89_mv2.jpg" alt="">
+                <h4><a href="blog-details.php">Diğer ve daha yaygın adıyla rent a car hizmeti, tüm dünyada standart olarak süreye göre sınıflandırılır, bunlardan en yaygını günlük araç kiralamadır.</a></h4>
+                <p>Günlük araba kiralama sürecinde rent a car markasının size sunduğu müsait araçlar arasından seçim yaparak; temiz ve bakımlı aracınızı rezervasyon tarihinde teslim alırsınız.Kiralama sürecinin sonunda aracı önceden belirttiğiniz tarihte ve belirtilen kiralama ofisine teslim edersiniz. Bazı hizmet paketlerinde araç, şirket yetkilisi tarafından size getirilip yine şirket yetkilisi tarafından belirttiğiniz yerden teslim alınabilir.</p>
+              </article>
+              <article id='tabs-5'>
+                <img src="assets/images/Eng6qMNWEAYebZb.jpg" alt="">
+                <h4><a href="blog-details.php">Karşılaştırmayı yaparken araçların teknik özelliklerine göre tercihinizi filtreleyebilirsiniz.</a></h4>
+                <p>Manuel ya da otomatik vitesli, dizel ya da benzinli olarak seçebileceğiniz araçları ekonomik, orta ve üst segmentler altında görüntüleyebilirsiniz. Araç karşılaştırması yaparken fiyat ve performans odaklı sonuçları göz önünde bulundurmak size avantaj sağlayacaktır.Özel günler ve prestijli toplantılar için lüks araçlar arasından da tercihinizi yapabilirsiniz. İhtiyacınıza göre sıralanmış araç kiralama fiyatları arasından yapacağınız seçimin ardından, ideal aracınız size istediğiniz noktada rent a car firması aracılığı ile güvenle teslim edilir.</p>
+              </article>
+            </section>
+          </div>
+        </div>  
+      </div>
+    </div>
+    <br><br>
+    <br><br>
+    <br><br>
+  
+    <!-- Footer Starts Here -->
+    <?php require_once 'footer.php'; ?>
+    
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Additional Scripts -->
+    <script src="assets/js/custom.js"></script>
+    <script src="assets/js/owl.js"></script>
+    <script src="assets/js/slick.js"></script>
+    <script src="assets/js/accordions.js"></script>
+
+    <script language = "text/Javascript"> 
+      cleared[0] = cleared[1] = cleared[2] = 0; //set a cleared flag for each field
+      function clearField(t){                   //declaring the array outside of the
+      if(! cleared[t.id]){                      // function makes it static and global
+          cleared[t.id] = 1;  // you could use true and false, but that's more typing
+          t.value='';         // with more chance of typos
+          t.style.color='#fff';
+          }
+      }
+    </script>
+
+  </body>
+</html>
