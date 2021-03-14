aa94757f78c043d6979c02e86325a0edcc1bde06
Beyza Subaşı <44063374+beyzasubasi@users.noreply.github.com>
2021-03-14 11:39:11 +0300

    Create about.php

---
+++ about.php
@@ -0,0 +1,165 @@
+<?php require_once 'baglan.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+  </head>
+
+  <body>
+
+    <!-- ***** Preloader Start ***** -->
+    <div id="preloader">
+        <div class="jumper">
+            <div></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>  
+    <!-- ***** Preloader End ***** -->
+
+    <!-- Header -->
+    <div class="sub-header">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-8 col-xs-12">
+            <ul class="left-info">
+              <li><a href="#"><i class="fa fa-envelope"></i>ventacav@company.com</a></li>
+              <li><a href="#"><i class="fa fa-phone"></i>000-000-0000</a></li>
+            </ul>
+          </div>
+          <div class="col-md-4">
+            <ul class="right-icons">
+              <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+              <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+              <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <?php require_once 'header.php'; ?>
+
+    <!-- Page Content -->
+    <div class="page-heading header-text">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <h1 style="color: #fff;">HAKKIMIZDA</h1>
+            <span style="color: #fff;">1 AYLIK TECRÜBEMİZLE YANINIZDAYIZ..</span>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="more-info about-info">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="more-info-content">
+              <div class="row">
+                <div class="col-md-6 align-self-center">
+                  <div class="right-content">
+                    <span>VENTACAV ARAÇ KİRALAMA.AŞ</span>
+                    <h2>PEKİ <em>ŞİRKETİMİZ</em> SİZLERE NASIL BİR HİZMET SUNUYOR?</h2>
+                    <p>Müşteri memnuniyeti odaklı özel hizmet anlayışı ve çözüm odaklı yaklaşımı ile sektörde her zaman öne çıkan VentaCav, uzun dönem filo kiralama alanında da kurumsal firmalar için birçok avantaj sunmaktadır. Türkiye’nin lider birçok kurumsal şirketi ile çalışan Vent a Cav, hem fiyatta hem de hizmette her zaman müşterileri için en iyiyi sunmaktadır. 
+                    <br><br>VentaCav Şubeleri’ne her zaman bir telefon kadar yakın olursunuz. Şubelerimize uzak olduğunuz ya da zamanınızın kısıtlı olduğu durumlardaysa adrese teslim hizmetinden yararlanarak kiraladığınız aracın bulunduğunuz adrese gelmesini isteyebilirsiniz. Bunun yanında çağrı merkezlerimiz de her durumda sorularınızı yanıtlamak ve beklenmedik durumlarda size yardımcı olmak için hizmetinizdedir. </p>
+                  </div>
+                </div>
+                <div class="col-md-6">
+                  <div class="left-image">
+                    <img src="assets/images/7dc0aabb8ea9b7138c35c3b31e18374d--matte-black-dreams.jpg" alt="">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="fun-facts">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="left-content">
+              
+              <h2>BAŞARILI İSTATİSTİKLERİMİZLE <em>YANINIZDAYIZ</em></h2>
+              <p>Yepyeni bir araç kiralama deneyimi anlayışı ile çok kısa sürede önemli atılımlar yaparak büyüyen markamız, her geçen gün artan hizmet noktaları ile müşterilerine kaliteden ödün vermeden hizmet vermeye devam etmektedir.100.000 mil sürüş, memnuniyetiyle yanımızda yer alan 1280 müşterimiz ile 12 şehirde 6 model aracımızla her zaman yanınızdayız. 
+              <br><br>
+              <span>KALİTE TESADÜF DEĞİLDİR!</span>
+              </p>
+            </div>
+          </div>
+          <div class="col-md-6 align-self-center">
+            <div class="row">
+              <div class="col-md-6">
+                <div class="count-area-content">
+                  <div class="count-digit">100000</div>
+                  <div class="count-title">Mil Sürüş</div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="count-area-content">
+                  <div class="count-digit">1280</div>
+                  <div class="count-title">Memnun Müşteri</div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="count-area-content">
+                  <div class="count-digit">12</div>
+                  <div class="count-title">Şehir</div>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="count-area-content">
+                  <div class="count-digit">6</div>
+                  <div class="count-title">ARABA</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Footer Starts Here -->
+    <?php require_once 'footer.php'; ?>
+    
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Additional Scripts -->
+    <script src="assets/js/custom.js"></script>
+    <script src="assets/js/owl.js"></script>
+    <script src="assets/js/slick.js"></script>
+    <script src="assets/js/accordions.js"></script>
+
+    <script language = "text/Javascript"> 
+      cleared[0] = cleared[1] = cleared[2] = 0; //set a cleared flag for each field
+      function clearField(t){                   //declaring the array outside of the
+      if(! cleared[t.id]){                      // function makes it static and global
+          cleared[t.id] = 1;  // you could use true and false, but that's more typing
+          t.value='';         // with more chance of typos
+          t.style.color='#fff';
+          }
+      }
+    </script>
+
+  </body>
+</html>
