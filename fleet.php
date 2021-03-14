b8a9636edb5501be51c28902471c168022c90489
Beyza Subaşı <44063374+beyzasubasi@users.noreply.github.com>
2021-03-14 11:42:13 +0300

    Create fleet.php

---
+++ fleet.php
@@ -0,0 +1,421 @@
+<?php require_once 'baglan.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link href="https://fonts.googleapis.com/css?family=Poppins:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Additional CSS Files -->
+  <link rel="stylesheet" href="assets/css/fontawesome.css">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="assets/css/owl.css">
+</head>
+
+<body>
+
+  <!-- ***** Preloader Start ***** -->
+  <div id="preloader">
+    <div class="jumper">
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  </div>  
+  <!-- ***** Preloader End ***** -->
+
+  <!-- Header -->
+  <div class="sub-header">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-8 col-xs-12">
+          <ul class="left-info">
+            <li><a href="#"><i class="fa fa-envelope"></i>ventacav@company.com</a></li>
+            <li><a href="#"><i class="fa fa-phone"></i>000-000-0000</a></li>
+          </ul>
+        </div>
+        <div class="col-md-4">
+          <ul class="right-icons">
+            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+  <?php require_once 'header.php'; ?>
+
+  <!-- Page Content -->
+  <div class="page-heading header-text">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <h1 style="color: #fff;">FİLOMUZ</h1>
+          <span style="color: #fff;">ULTRA LÜKS ARAÇLARIMIZLA HİZMETİNİZDEYİZ</span>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="services">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+          <div class="service-item">
+            <img src="assets/images/EnHS8EEVkAYTPr1.jpg" alt="">
+            <div class="down-content">
+              <div style="margin-bottom:10px;">
+                <b>MODEL:</b><h4>MERCEDES-AMG GT 4.0 R</h4>
+                <p><b>GÜNLÜK KİRA BEDELİ:</b> 1000 TL</p>
+                <p><b>MOTOR GÜCÜ:</b><!--<?php echo $row['motor_gucu']; ?></p>-->
+                <p><b>MOTOR HACMİ:</b> 3982 cc</p>
+                <p><b>YAKIT:</b> benzin</p>
+                <p><b>KASA TİPİ:</b> coupe</p>
+                <p><b>VİTES:</b> otomatik</p>
+                <p><b>KM:</b> 35000</p>
+                <p><b>ÇEKİŞ:</b> arkadan itiş</p>
+                <p><b>KAPI:</b> 4</p>
+
+              </div>
+              <p>
+                <i class="fa fa-user" title="passegengers"></i> 5 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-briefcase" title="luggages"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-sign-out" title="doors"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-cog" title="transmission"></i> 
+              </p>
+              <!--<a href="#" data-toggle="modal" data-target="#exampleModal" class="filled-button">İNCELE</a> -->
+            </div>
+          </div>
+
+          <br>
+        </div>
+
+        <div class="col-md-4">
+          <div class="service-item">
+            <img src="assets/images/1b.jpg" alt="">
+            <div class="down-content">
+              <div style="margin-bottom:10px;">
+                <b>MODEL:</b><h4>LAMBORGHINI-URUS 4.0</h4>
+                <p><b>GÜNLÜK KİRA BEDELİ:</b> 2400 TL</p>
+                <p><b>MOTOR GÜCÜ:</b> 650 hp</p>
+                <p><b>MOTOR HACMİ:</b> 3996 cc</p>
+                <p><b>YAKIT:</b> benzin</p>
+                <p><b>KASA TİPİ:</b> SUV</p>
+                <p><b>VİTES:</b> otomatik</p>
+                <p><b>KM:</b> 580</p>
+                <p><b>ÇEKİŞ:</b> 4X4</p>
+                <p><b>KAPI:</b> 5</p>
+              </div>
+              <p>
+                <i class="fa fa-user" title="passegengers"></i> 5 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-briefcase" title="luggages"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-sign-out" title="doors"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-cog" title="transmission"></i> 
+              </p>
+              <!--<a href="#" data-toggle="modal" data-target="#exampleModal" class="filled-button">İNCELE</a> -->
+            </div>
+          </div>
+
+          <br>
+        </div>
+
+        <div class="col-md-4">
+          <div class="service-item">
+            <img src="assets/images/EozpNN-XMAIMTl7.jpg" alt="">
+            <div class="down-content">
+              <div style="margin-bottom:10px;">
+                <b>MODEL:</b><h4>BMW-2 216D GRAN COUPE</h4>
+                <p><b>GÜNLÜK KİRA BEDELİ:</b> 800 TL</p>
+                <p><b>MOTOR GÜCÜ:</b> 116 hp</p>
+                <p><b>MOTOR HACMİ:</b> 1496 cc</p>
+                <p><b>YAKIT:</b> benzin</p>
+                <p><b>KASA TİPİ:</b> coupe</p>
+                <p><b>VİTES:</b> otomatik</p>
+                <p><b>KM:</b> 1000</p>
+                <p><b>ÇEKİŞ:</b> önden çekiş</p>
+                <p><b>KAPI:</b> 4</p>
+              </div>
+              <p>
+                <i class="fa fa-user" title="passegengers"></i> 5 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-briefcase" title="luggages"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-sign-out" title="doors"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-cog" title="transmission"></i> 
+              </p>
+              <!--<a href="#" data-toggle="modal" data-target="#exampleModal" class="filled-button">İNCELE</a> -->
+            </div>
+          </div>
+
+          <br>
+        </div>
+
+        <div class="col-md-4">
+          <div class="service-item">
+            <img src="assets/images/2b.jpg" alt="">
+            <div class="down-content">
+              <div style="margin-bottom:10px;">
+                <b>MODEL:</b><h4>ASTON MARTIN DB11 COUPE</h4>
+                <p><b>GÜNLÜK KİRA BEDELİ:</b> 1600 TL</p>
+                <p><b>MOTOR GÜCÜ:</b> 601 hp</p>
+                <p><b>MOTOR HACMİ:</b> 55000 cm<sup>3</sup></p>
+                <p><b>YAKIT:</b> benzin</p>
+                <p><b>KASA TİPİ:</b> coupe</p>
+                <p><b>VİTES:</b> otomatik</p>
+                <p><b>KM:</b> 0</p>
+                <p><b>ÇEKİŞ:</b> arkadan itiş</p>
+                <p><b>KAPI:</b> 5</p>
+              </div>
+              <p>
+                <i class="fa fa-user" title="passegengers"></i> 5 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-briefcase" title="luggages"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-sign-out" title="doors"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-cog" title="transmission"></i> 
+              </p>
+              <!--<a href="#" data-toggle="modal" data-target="#exampleModal" class="filled-button">İNCELE</a> -->
+            </div>
+          </div>
+
+          <br>
+        </div>
+
+        <div class="col-md-4">
+          <div class="service-item">
+            <img src="assets/images/2b2f35c511fef6fb5a9cccc8210e6816.jpg" alt="">
+            <div class="down-content">
+              <div style="margin-bottom:10px;">
+                <b>MODEL:</b><h4>RANGE ROVER 3.0 SDV6 HSE</h4>
+                <p><b>GÜNLÜK KİRA BEDELİ:</b> 1200 TL</p>
+                <p><b>MOTOR GÜCÜ:</b> 245 hp</p>
+                <p><b>MOTOR HACMİ:</b> 3982 cc</p>
+                <p><b>YAKIT:</b> benzin</p>
+                <p><b>KASA TİPİ:</b> SUV</p>
+                <p><b>VİTES:</b> otomatik</p>
+                <p><b>KM:</b> 0</p>
+                <p><b>ÇEKİŞ:</b> 4X4</p>
+                <p><b>KAPI:</b> 5</p>
+              </div>
+              <p>
+                <i class="fa fa-user" title="passegengers"></i> 5 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-briefcase" title="luggages"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-sign-out" title="doors"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-cog" title="transmission"></i> 
+              </p>
+              <!--<a href="#" data-toggle="modal" data-target="#exampleModal" class="filled-button">İNCELE</a> -->
+            </div>
+          </div>
+
+          <br>
+        </div>
+
+        <div class="col-md-4">
+          <div class="service-item">
+            <img src="assets/images/aa.jpg" alt="">
+            <div class="down-content">
+              <div style="margin-bottom:10px;">
+                <b>MODEL:</b><h4>AUDI R8 4.2 FSI QUATTRO</h4>
+                <p><b>GÜNLÜK KİRA BEDELİ:</b> 900 TL</p>
+                <p><b>MOTOR GÜCÜ:</b> 430 hp</p>
+                <p><b>MOTOR HACMİ:</b> 4163 cc</p>
+                <p><b>YAKIT:</b> benzin</p>
+                <p><b>KASA TİPİ:</b> coupe</p>
+                <p><b>VİTES:</b> otomatik</p>
+                <p><b>KM:</b> 4500</p>
+                <p><b>ÇEKİŞ:</b> 4WD</p>
+                <p><b>KAPI:</b> 2</p>
+              </div>
+              <p>
+                <i class="fa fa-user" title="passegengers"></i> 5 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-briefcase" title="luggages"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-sign-out" title="doors"></i> 4 &nbsp;&nbsp;&nbsp;
+                <i class="fa fa-cog" title="transmission"></i> 
+              </p>
+              <!--<a href="#" data-toggle="modal" data-target="#exampleModal" class="filled-button">İNCELE</a> -->
+            </div>
+          </div>
+
+          <br>
+        </div>
+      </div>
+
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+      <br>
+    </div>
+  </div>
+
+  <!-- Footer Starts Here -->
+  <?php require_once 'footer.php'; ?>
+
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" style="margin-top: 70px;">
+    <div class="modal-dialog modal-lg" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">İNCELE</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <form action="#" id="contact">
+            <div class="row">
+             <div class="col-md-6">
+              <div class="form-group">
+                <fieldset>
+                  <p><b>MOTOR:</b> V8</p>
+                </fieldset>
+              </div>
+            </div>
+
+            <div class="col-md-6">
+              <div class="form-group">
+                <fieldset>
+                  
+                </fieldset>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+           <div class="col-md-6">
+            <div class="form-group">
+              <fieldset>
+                <p><b>MOTOR:</b> V8</p>
+              </fieldset>
+            </div>
+          </div>
+
+          <div class="col-md-6">
+            <div class="form-group">
+              <fieldset>
+                
+              </fieldset>
+            </div>
+          </div>
+        </div>
+
+        <div class="form-group">
+          <fieldset>
+            <p><b>MOTOR:</b> V8</p>
+          </fieldset>
+        </div>
+
+        <div class="row">
+         <div class="col-md-6">
+          <div class="form-group">
+            <fieldset>
+              <p><b>MOTOR:</b> V8</p>
+            </fieldset>
+          </div>
+        </div>
+
+        <div class="col-md-6">
+          <div class="form-group">
+            <fieldset>
+              
+            </fieldset>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+       <div class="col-md-6">
+        <div class="form-group">
+          <fieldset>
+            <p><b>MOTOR:</b> V8</p>
+          </fieldset>
+        </div>
+      </div>
+
+      <div class="col-md-6">
+        <div class="form-group">
+          <fieldset>
+            
+          </fieldset>
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+     <div class="col-md-6">
+      <div class="form-group">
+        <fieldset>
+          <p><b>MOTOR:</b> V8</p>
+        </fieldset>
+      </div>
+    </div>
+
+    <div class="col-md-6">
+      <div class="form-group">
+        <fieldset>
+          
+        </fieldset>
+      </div>
+    </div>
+  </div>
+
+  <div class="form-group">
+    <fieldset>
+      <p><b>MOTOR:</b> V8</p>
+    </fieldset>
+  </div>
+
+  <div class="row">
+   <div class="col-md-6">
+    <div class="form-group">
+      <fieldset>
+        <p><b>MOTOR:</b> V8</p>
+      </fieldset>
+    </div>
+  </div>
+
+  <div class="col-md-6">
+    <div class="form-group">
+      <fieldset>
+        
+      </fieldset>
+    </div>
+  </div>
+</div>
+</form>
+</div>
+<div class="modal-footer">
+  <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+</div>
+</div>
+</div>
+</div>
+
+<!-- Bootstrap core JavaScript -->
+<script src="vendor/jquery/jquery.min.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<!-- Additional Scripts -->
+<script src="assets/js/custom.js"></script>
+<script src="assets/js/owl.js"></script>
+<script src="assets/js/slick.js"></script>
+<script src="assets/js/accordions.js"></script>
+
+<script language = "text/Javascript"> 
+      cleared[0] = cleared[1] = cleared[2] = 0; //set a cleared flag for each field
+      function clearField(t){                   //declaring the array outside of the
+      if(! cleared[t.id]){                      // function makes it static and global
+          cleared[t.id] = 1;  // you could use true and false, but that's more typing
+          t.value='';         // with more chance of typos
+          t.style.color='#fff';
+        }
+      }
+    </script>
+
+  </body>
+  </html>
