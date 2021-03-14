c012f5bfbda5a526dd70c2dfc18975898ea91de0
Beyza Subaşı <44063374+beyzasubasi@users.noreply.github.com>
2021-03-14 11:42:37 +0300

    Create message.php

---
+++ message.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+
+<?php
+require_once 'baglan.php';
+
+ini_set("display_errors",1);//htaları gilzer
+
+if($_POST["messages"] == "add")
+{
+	//var_dump($_POST);die;
+	$kaydet=$db->prepare("INSERT INTO message SET
+		ad_soyad =:ad_soyad,
+		mail	 =:mail,
+		konu     =:konu,
+		mesaj    =:mesaj
+		
+		");
+
+
+	$insert=$kaydet->execute(array(
+		'ad_soyad' => $_POST['ad_soyad'],
+		'mail'     => $_POST['mail'],
+		'konu'     => $_POST['konu'],
+		'mesaj'    => $_POST['mesaj']
+	
+		
+	));
+
+	if ($insert)
+	{
+		Header("Location:contact.php?durum=ok");
+	}
+
+	else
+	{
+		Header("Location:contact.php?durum=no");
+	}
+}
+/*
+else if($_POST["islem"] == "uyeGiris"){
+	echo "burda";
+}
+*/
+//elif($_POST["islem"] == "ekle")  //buraya mesaj için olan değişkenleri koy elifle girsin değişkenler indexte var
+ //{
+
+?>
+ 
+
+</body>
+</html>
+
+
